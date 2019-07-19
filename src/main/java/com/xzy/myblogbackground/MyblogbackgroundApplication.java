583da6b0d8b3230c1327f2035c716ad91275591c
xuzy1996 <xuzey1996@163.com>
2019-07-19 16:16:57 +0800

    first commit

---
+++ src/main/java/com/xzy/myblogbackground/MyblogbackgroundApplication.java
@@ -0,0 +1,13 @@
+package com.xzy.myblogbackground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyblogbackgroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyblogbackgroundApplication.class, args);
+    }
+
+}
