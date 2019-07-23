be910c6526071e967807ca131e586b6b284c7d75
xuzy1996 <xuzey1996@163.com>
2019-07-23 10:50:43 +0800

    second commit

--- src/main/java/com/xzy/myblogbackground/MyblogbackgroundApplication.java
+++ src/main/java/com/xzy/myblogbackground/MyblogbackgroundApplication.java
@@ -1,9 +1,11 @@
 package com.xzy.myblogbackground;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = {"com.xzy.myblogbackground.mapper"})
 public class MyblogbackgroundApplication {
 
     public static void main(String[] args) {
